5c7a4d070bf57ff3bdd461816e68a2807c01499b
int_lyc <int_lyc@outlook.com>
2019-03-30 18:40:24 +0800

    change name

--- JAVA/BigNumber/BigNumber.java
+++ JAVA/BigNumber/BigNumber.java
@@ -1,20 +1,20 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class BigNumber {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i<x; i++){
+        for (int i = 0; i<x; i++) {
             String sa = sc.next();
             String sb = sc.next();
             BigInteger a = new BigInteger(sa);
             BigInteger b = new BigInteger(sb);
-            if (i == x-1){
+            if (i == x-1) {
                 System.out.println("Case "+(i+1)+":");
                 System.out.println(a+" + "+b+" = "+a.add(b));
             }
-            else{
+            else {
                 System.out.println("Case "+(i+1)+":");
                 System.out.println(a+" + "+b+" = "+a.add(b));
                 System.out.println();
