4035f9b6a0d140a9119c7907cc051d2921e178b4
int_lyc <int_lyc@outlook.com>
2019-05-31 00:05:07 +0800

    2019/5/31

--- JAVA/Test/Test.java
+++ JAVA/Test/Test.java
@@ -1,9 +1,8 @@
 public class Test {
     public static void main(String[] args) {
+        double max = 5000000;
+        double x, y;
         // 蒙特卡洛法
-        int max = 5000000;
-        double x;
-        double y;
         int num = 0;
         for (int i=0; i<max; i++) {
             x = Math.random();
@@ -12,14 +11,16 @@ public class Test {
         }
         System.out.println(num/max);
         // 分割矩形法
-        double max = 5000000;
         double w;
         double l;
         w = 1/max;
-        double x = w/2;
+        x = w/2;
+        // w = 5/max;
+        // x = 1+w/2;
         double sum = 0;
         for (int i=0; i<max; i++) {
             l = Math.sqrt(x) - x*x;
+            // l = 0.0167*Math.pow(x,5)-0.2917*Math.pow(x,4)+2*Math.pow(x,3)-6.7083*x*x+9.9833*x+61;
             sum += w*l;
             x += w;
         }
