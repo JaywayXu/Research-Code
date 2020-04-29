34c56adb2b39108474d0ec567b0d1fcbbce026d4
int-lyc <int_lyc@outlook.com>
2020-04-29 14:37:52 +0800

    添加了上传完文件识别后就删除

--- SoftwareInternship/app/views/recgnize.py
+++ SoftwareInternship/app/views/recgnize.py
@@ -23,6 +23,7 @@ class Rec():
     def getPredict(self, img_path):
         '''获取预测车牌号'''
         cv_img = cv2.imread(img_path)
+        os.remove(img_path)
         for pstr, confidence, rect in self.model.SimpleRecognizePlateByE2E(cv_img):
             if confidence > 0.7:  # 若置信度大于0.7，则识别结果可信(最大为1)
                 return pstr, confidence
