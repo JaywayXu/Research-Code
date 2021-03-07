3cced48ad6c2e54424f2c1d0a49d0c02b9581e41
int-lyc <int_lyc@outlook.com>
2021-03-07 15:30:56 +0800

    1

---
+++ UndergraduateCourses/4GraduationThesis/GBO_Python/Manage.py
@@ -0,0 +1,20 @@
+from BenchmarkFunctions import BenchmarkFunctions
+from Optimizer import GBO
+
+
+class Manage:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    nP = 50
+    MaxIt = 500
+    fname = "F11"
+
+    bf = BenchmarkFunctions()
+    lb, ub, dim, fobj = bf.get(fname)
+
+    gbo = GBO(nP, MaxIt, lb, ub, dim, fobj)
+    Best_Cost, Best_X, Convergence_curve = gbo.get_resault()
+    print(Best_Cost, Best_X)
