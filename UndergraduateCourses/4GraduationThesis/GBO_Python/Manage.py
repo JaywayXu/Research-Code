e5f55b0a683ddacff1ce0b181f28e85ba04bbce1
int-lyc <int_lyc@outlook.com>
2021-03-08 21:21:10 +0800

    draw function

--- UndergraduateCourses/4GraduationThesis/GBO_Python/Manage.py
+++ UndergraduateCourses/4GraduationThesis/GBO_Python/Manage.py
@@ -1,13 +1,14 @@
 from BenchmarkFunctions import BenchmarkFunctions
 from Optimizer import GBO
 import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class Manage:
     def __init__(self):
         self.nP = 50
-        self.MaxIt = 50
-        self.fname = "F1"
+        self.MaxIt = 300
         self.bmf = BenchmarkFunctions()
         self.testNum = 10
 
@@ -24,12 +25,47 @@ class Manage:
                 gbo = GBO(self.nP, self.MaxIt, lb, ub, nV, fobj)
                 np_Best_Cost[test_i], np_Best_X[test_i], np_Convergence_curve[
                     test_i] = gbo.get_resault()
-            
+
             ave_Best_Cost = np_Best_Cost.mean()
             print(ave_Best_Cost)
             input()
 
+    def drawFunction_1D(self, fname):
+        lb, ub, nV, fobj = self.bmf.get(fname)
+
+        figure = plt.figure()
+        axes = Axes3D(figure)
+        X = np.arange(lb, ub, 1)
+        Y = np.arange(lb, ub, 1)
+
+        X, Y = np.meshgrid(X, Y)
+        Z = np.zeros(X.shape)
+
+        for i in range(X.shape[0]):
+            for j in range(X.shape[1]):
+                Z[i, j] = fobj(np.array([X[i, j], Y[i, j]]))
+        axes.plot_surface(X, Y, Z, cmap='rainbow')
+        plt.show()
+
+    def drawFunction_2D(self, fname):
+        lb, ub, nV, fobj = self.bmf.get(fname)
+
+        figure = plt.figure()
+        axes = Axes3D(figure)
+        X = np.arange(lb, ub, 1)
+        Y = np.arange(lb, ub, 1)
+
+        X, Y = np.meshgrid(X, Y)
+        Z = np.zeros(X.shape)
+
+        for i in range(X.shape[0]):
+            for j in range(X.shape[1]):
+                Z[i, j] = fobj(np.array([X[i, j], Y[i, j]]))
+        axes.plot_surface(X, Y, Z, cmap='rainbow')
+        plt.show()
+
 
 if __name__ == "__main__":
     mng = Manage()
-    mng.run_GBO()
+    # mng.run_GBO()
+    mng.drawFunction_2D(11)
