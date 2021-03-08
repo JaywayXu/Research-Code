b7489554cbc5dbcec1cd716339d977f631d5a891
int-lyc <int_lyc@outlook.com>
2021-03-08 14:30:57 +0800

    format

--- UndergraduateCourses/4GraduationThesis/GBO_Python/BenchmarkFunctions.py
+++ UndergraduateCourses/4GraduationThesis/GBO_Python/BenchmarkFunctions.py
@@ -126,7 +126,7 @@ class BenchmarkFunctions:
     def f1_obj(self, x):
         '''Bent Cigar (Unimodal)'''
         D = x.shape[0]
-        z = np.power(x[1], 2)+pow(10, 6)*np.power(x[2: D], 2).sum()
+        z = np.power(x[1], 2) + pow(10, 6) * np.power(x[2:D], 2).sum()
         return z
 
     def f2_obj(self, x):
@@ -134,29 +134,30 @@ class BenchmarkFunctions:
         D = x.shape[0]
         f = np.zeros([D, 1])
         for i in range(D):
-            f[i] = np.power(abs(x[i]), (i+1))
+            f[i] = np.power(abs(x[i]), (i + 1))
         z = f.sum()
         return z
 
     def f3_obj(self, x):
         '''Zakharov (Unimodal)'''
-        z = np.power(x, 2).sum() + pow((x.sum()*0.5), 2)+pow((x.sum()*0.5), 4)
+        z = np.power(x, 2).sum() + pow((x.sum() * 0.5), 2) + pow(
+            (x.sum() * 0.5), 4)
         return z
 
     def f4_obj(self, x):
         '''Rosenbrock (Unimodal)'''
         D = x.shape[0]
-        ff = np.zeros([D-1, 1])
-        for i in range(D-1):
-            ff[i] = 100 * np.power((np.power(x[i], 2)-x[i+1]),
-                                   2)+np.power((x[i]-1), 2)
+        ff = np.zeros([D - 1, 1])
+        for i in range(D - 1):
+            ff[i] = 100 * np.power(
+                (np.power(x[i], 2) - x[i + 1]), 2) + np.power((x[i] - 1), 2)
         z = ff.sum()
         return z
 
     def f5_obj(self, x):
         '''Discus (Unimodal)'''
         D = x.shape[0]
-        z = pow(10, 6) * np.power(x[0], 2) + np.power(x[1: D], 2).sum()
+        z = pow(10, 6) * np.power(x[0], 2) + np.power(x[1:D], 2).sum()
         return z
 
     def f6_obj(self, x):
@@ -164,7 +165,7 @@ class BenchmarkFunctions:
         D = x.shape[0]
         f = np.zeros([D, 1])
         for i in range(D):
-            f[i] = pow(pow(10, 6), (i-1)/(D-1)) * np.power(x[i], 2)
+            f[i] = pow(pow(10, 6), (i - 1) / (D - 1)) * np.power(x[i], 2)
         z = f.sum()
         return z
 
@@ -173,12 +174,15 @@ class BenchmarkFunctions:
         D = x.shape[0]
         f = np.zeros([D, 1])
         for i in range(D):
-            if i == D-1:
-                f[i] = 0.5 + (pow(np.sin(np.sqrt(pow(x[i], 2) + pow(x[0], 2))),
-                                  2) - 0.5) / pow(1 + 0.001*(pow(x[i], 2) + pow(x[0], 2)), 2)
+            if i == D - 1:
+                f[i] = 0.5 + (
+                    pow(np.sin(np.sqrt(pow(x[i], 2) + pow(x[0], 2))), 2) -
+                    0.5) / pow(1 + 0.001 * (pow(x[i], 2) + pow(x[0], 2)), 2)
             else:
-                f[i] = 0.5 + (pow(np.sin(np.sqrt(pow(x[i], 2) + pow(x[i+1], 2))),
-                                  2) - 0.5) / pow(1 + 0.001*(pow(x[i], 2) + pow(x[i+1], 2)), 2)
+                f[i] = 0.5 + (
+                    pow(np.sin(np.sqrt(pow(x[i], 2) + pow(x[i + 1], 2))), 2) -
+                    0.5) / pow(1 + 0.001 *
+                               (pow(x[i], 2) + pow(x[i + 1], 2)), 2)
         z = f.sum()
         return z
 
@@ -186,11 +190,12 @@ class BenchmarkFunctions:
         '''Levy Function (Multimodal)'''
         D = x.shape[0]
         w = np.zeros([D, 1])
-        f = np.zeros([D-1, 1])
-        for i in range(D-1):
-            w[i] = 1+(x[i]-1)/4
-            f[i] = pow((w[i]-1), 2) * (1 + 10 * pow(np.sin(np.pi*w[i]+1), 2))
-        w[D-1] = 1 + (x[D-1]-1) / 4
+        f = np.zeros([D - 1, 1])
+        for i in range(D - 1):
+            w[i] = 1 + (x[i] - 1) / 4
+            f[i] = pow(
+                (w[i] - 1), 2) * (1 + 10 * pow(np.sin(np.pi * w[i] + 1), 2))
+        w[D - 1] = 1 + (x[D - 1] - 1) / 4
         z = pow(np.sin(np.pi*w[0]), 2) + f.sum() + \
             pow((w[D-1]-1), 2) * (1 + pow(np.sin(2*np.pi*w[D-1]), 2))
         return z
@@ -200,9 +205,9 @@ class BenchmarkFunctions:
         D = x.shape[0]
         f = np.zeros([D, 1])
         for i in range(D):
-            y = x[i]+4.209687462275036e+002
+            y = x[i] + 4.209687462275036e+002
             if abs(y) < 500:
-                f[i] = y*np.sin(pow(abs(y), 0.5))
+                f[i] = y * np.sin(pow(abs(y), 0.5))
             elif y > 500:
                 f[i] = (500 - y % 500) * np.sin(np.sqrt(abs(500 - y % 500))) - \
                     pow((y-500), 2)/(10000*D)
@@ -224,6 +229,7 @@ class BenchmarkFunctions:
         def w(x1, c1, c2):
             y = (c1 * np.cos(c2 * x1)).sum()
             return y
+
         D = x.shape[0]
         x = x + 0.5
         a = 0.5
@@ -231,8 +237,8 @@ class BenchmarkFunctions:
         kmax = 20
         c1 = np.zeros([20])
         c2 = np.zeros([20])
-        c1[0: kmax] = np.power(a, range(kmax))
-        c2[0: kmax] = 2 * np.pi * np.power(b, range(kmax))
+        c1[0:kmax] = np.power(a, range(kmax))
+        c2[0:kmax] = 2 * np.pi * np.power(b, range(kmax))
         f = 0
         c = -w(0.5, c1, c2)
         for i in range(D):
@@ -251,10 +257,10 @@ class BenchmarkFunctions:
         '''HGBat Function (Multimodal)'''
         D = x.shape[0]
         z = pow(abs(pow(np.power(x, 2).sum(), 2) - pow(x.sum(), 2)),
-                1/2) + (0.5 * np.power(x, 2).sum() + x.sum()) / D + 0.5
+                1 / 2) + (0.5 * np.power(x, 2).sum() + x.sum()) / D + 0.5
         return z
 
     def f14_obj(self, x):
         '''Alpine (Multimodal)'''
-        z = sum(abs(x*np.sin(x) + 0.1*x))
+        z = sum(abs(x * np.sin(x) + 0.1 * x))
         return z
--- UndergraduateCourses/4GraduationThesis/GBO_Python/Optimizer/GBO.py
+++ UndergraduateCourses/4GraduationThesis/GBO_Python/Optimizer/GBO.py
@@ -15,7 +15,8 @@ class GBO:
         '''Run the Optimize Algorithm'''
         self.Cost = np.zeros([self.nP, 1])
         # Initialize the set of random solutions
-        self.X = self.lb + (self.ub-self.lb) * np.random.rand(self.nP, self.nV)
+        self.X = self.lb + (self.ub - self.lb) * np.random.rand(
+            self.nP, self.nV)
         Convergence_curve = np.zeros([1, self.MaxIt])
 
         # Calculate the Value of Objective Function
@@ -33,64 +34,71 @@ class GBO:
         # Main Loop
         for it in range(self.MaxIt):
             # Eq.(14.2)
-            beta = 0.2+(1.2-0.2) * pow((1-pow((it/self.MaxIt), 3)), 2)
+            beta = 0.2 + (1.2 - 0.2) * pow((1 - pow((it / self.MaxIt), 3)), 2)
             # Eq.(14.1)
-            alpha = abs(beta * np.sin((3*np.pi/2 + np.sin(3*np.pi/2*beta))))
+            alpha = abs(beta * np.sin(
+                (3 * np.pi / 2 + np.sin(3 * np.pi / 2 * beta))))
 
             for i in range(self.nP):
                 # Four positions randomly selected from population
-                A1 = np.fix(np.random.rand(1, self.nP)*self.nP)
+                A1 = np.fix(np.random.rand(1, self.nP) * self.nP)
                 r1, r2, r3, r4 = A1[:, 0:4][0, :].astype(np.int16)
 
                 # Average of Four positions randomly selected from population
-                Xm = (self.X[r1, :]+self.X[r2, :] +
-                      self.X[r3, :]+self.X[r4, :])/4
-                ro = alpha * (2*np.random.rand()-1)
-                ro1 = alpha * (2*np.random.rand()-1)
+                Xm = (self.X[r1, :] + self.X[r2, :] + self.X[r3, :] +
+                      self.X[r4, :]) / 4
+                ro = alpha * (2 * np.random.rand() - 1)
+                ro1 = alpha * (2 * np.random.rand() - 1)
                 eps = 5e-3 * np.random.rand()  # Randomization Epsilon
 
                 # Direction of Movement Eq.(18)
-                DM = np.random.rand() * ro * (Best_X-self.X[r1, :])
+                DM = np.random.rand() * ro * (Best_X - self.X[r1, :])
                 Flag = 1
-                GSR = self.GradientSearchRule(
-                    ro1, Best_X, Worst_X, self.X[i, :], self.X[r1, :], DM, eps, Xm, Flag)
-                DM = np.random.rand() * ro * (Best_X-self.X[r1, :])
+                GSR = self.GradientSearchRule(ro1, Best_X, Worst_X,
+                                              self.X[i, :], self.X[r1, :], DM,
+                                              eps, Xm, Flag)
+                DM = np.random.rand() * ro * (Best_X - self.X[r1, :])
                 X1 = self.X[i, :] - GSR + DM  # Eq.(25)
 
                 Flag = 2
-                DM = np.random.rand() * ro * (self.X[r1, :]-self.X[r2, :])
-                GSR = self.GradientSearchRule(
-                    ro1, Best_X, Worst_X, self.X[i, :], self.X[r1, :], DM, eps, Xm, Flag)
-                DM = np.random.rand() * ro * (self.X[r1, :]-self.X[r2, :])
+                DM = np.random.rand() * ro * (self.X[r1, :] - self.X[r2, :])
+                GSR = self.GradientSearchRule(ro1, Best_X, Worst_X,
+                                              self.X[i, :], self.X[r1, :], DM,
+                                              eps, Xm, Flag)
+                DM = np.random.rand() * ro * (self.X[r1, :] - self.X[r2, :])
                 X2 = Best_X - GSR + DM  # Eq.(26)
 
                 Xnew = np.zeros([1, self.nV])
                 for j in range(self.nV):
-                    ro = alpha * (2*np.random.rand() - 1)
-                    X3 = self.X[i, j] - ro * (X2[0, j]-X1[0, j])
+                    ro = alpha * (2 * np.random.rand() - 1)
+                    X3 = self.X[i, j] - ro * (X2[0, j] - X1[0, j])
                     ra, rb = np.random.rand(), np.random.rand()
-                    Xnew[0, j] = ra * (rb * X1[0, j] + (1-rb) *
-                                       X2[0, j]) + (1-ra) * X3  # Eq.(27)
+                    Xnew[0,
+                         j] = ra * (rb * X1[0, j] +
+                                    (1 - rb) * X2[0, j]) + (1 -
+                                                            ra) * X3  # Eq.(27)
 
                 # Local escaping operator(LEO) # Eq.(28)
                 if np.random.rand() < self.pr:
                     k = np.fix(np.random.rand() * self.nP).astype(np.int16)
-                    f1 = -1 + (1-(-1)) * np.random.rand()
-                    f2 = -1 + (1-(-1)) * np.random.rand()
+                    f1 = -1 + (1 - (-1)) * np.random.rand()
+                    f2 = -1 + (1 - (-1)) * np.random.rand()
                     ro = alpha * (2 * np.random.rand() - 1)
                     Xk = self.lb + (self.ub-self.lb) * \
                         np.random.rand(1, self.nV)  # Eq.(28.8)
 
                     L1 = np.random.rand() < 0.5
-                    u1 = L1 * 2 * np.random.rand() + (1-L1)*1
-                    u2 = L1 * np.random.rand() + (1-L1)*1
-                    u3 = L1 * np.random.rand() + (1-L1)*1
+                    u1 = L1 * 2 * np.random.rand() + (1 - L1) * 1
+                    u2 = L1 * np.random.rand() + (1 - L1) * 1
+                    u3 = L1 * np.random.rand() + (1 - L1) * 1
                     L2 = np.random.rand() < 0.5
-                    Xp = (1-L2) * self.X[k, :] + L2 * Xk[0, :]  # Eq.(28.7)
+                    Xp = (1 - L2) * self.X[k, :] + L2 * Xk[0, :]  # Eq.(28.7)
 
                     if u1 < 0.5:
-                        Xnew = Xnew + f1 * (u1*Best_X - u2*Xp) + f2 * ro * (
-                            u3 * (X2-X1) + u2 * (self.X[r1, :] - self.X[r2, :])) / 2
+                        Xnew = Xnew + f1 * (
+                            u1 * Best_X - u2 * Xp) + f2 * ro * (
+                                u3 * (X2 - X1) + u2 *
+                                (self.X[r1, :] - self.X[r2, :])) / 2
                     else:
                         Xnew = Best_X + f1 * \
                             (u1*Best_X - u2*Xp) + f2*ro*(u3*(X2-X1) +
@@ -116,28 +124,33 @@ class GBO:
 
             # Show Iteration Information
             Convergence_curve[0, it] = Best_Cost
-            print('Iteration ', it+1, ': Best Fitness = ',
+            print('Iteration ', it + 1, ': Best Fitness = ',
                   Convergence_curve[0, it])
 
         return Best_Cost, Best_X, Convergence_curve
 
-    def GradientSearchRule(self, ro1, Best_X, Worst_X, X, Xr1, DM, eps, Xm, Flag):
+    def GradientSearchRule(self, ro1, Best_X, Worst_X, X, Xr1, DM, eps, Xm,
+                           Flag):
         '''Gradient Search Rule'''
 
         nV = X.shape[0]
-        Delta = 2 * np.random.rand() * abs(Xm-X)  # Eq.(16.2)
+        Delta = 2 * np.random.rand() * abs(Xm - X)  # Eq.(16.2)
         Step = ((Best_X - Xr1) + Delta) / 2  # Eq.(16.1)
         DelX = np.random.rand(1, nV) * (abs(Step))  # Eq.(16)
 
         # Gradient search rule  Eq.(15)
-        GSR = np.random.randn() * ro1 * (2 * DelX * X) / (Best_X - Worst_X + eps)
+        GSR = np.random.randn() * ro1 * (2 * DelX * X) / (Best_X - Worst_X +
+                                                          eps)
         if Flag == 1:
             Xs = X - GSR + DM  # Eq.(21)
         else:
             Xs = Best_X - GSR + DM
-        yp = np.random.rand() * (0.5 * (Xs+X) + np.random.rand() * DelX)  # Eq.(22.6)
-        yq = np.random.rand() * (0.5 * (Xs+X) - np.random.rand() * DelX)  # Eq.(22.7)
-        GSR = np.random.randn() * ro1 * (2*DelX*X) / (yp-yq+eps)  # Eq.(23)
+        yp = np.random.rand() * (0.5 * (Xs + X) + np.random.rand() * DelX
+                                 )  # Eq.(22.6)
+        yq = np.random.rand() * (0.5 * (Xs + X) - np.random.rand() * DelX
+                                 )  # Eq.(22.7)
+        GSR = np.random.randn() * ro1 * (2 * DelX * X) / (yp - yq + eps
+                                                          )  # Eq.(23)
         return GSR
 
 
@@ -152,7 +165,7 @@ if __name__ == '__main__':
     def f1(x):
         '''Bent Cigar (Unimodal)'''
         D = x.shape[0]
-        z = np.power(x[1], 2)+pow(10, 6)*np.power(x[2: D], 2).sum()
+        z = np.power(x[1], 2) + pow(10, 6) * np.power(x[2:D], 2).sum()
         return z
 
     def f2(x):
@@ -160,7 +173,7 @@ if __name__ == '__main__':
         D = x.shape[0]
         f = np.zeros([D, 1])
         for i in range(D):
-            f[i] = np.power(abs(x[i]), (i+1))
+            f[i] = np.power(abs(x[i]), (i + 1))
         z = f.sum()
         return z
 
