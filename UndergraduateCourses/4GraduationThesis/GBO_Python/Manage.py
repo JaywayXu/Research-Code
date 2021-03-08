7cee3a1ecbb2b865a3c3e2d749b00b17398fdd20
int-lyc <int_lyc@outlook.com>
2021-03-08 17:52:21 +0800

    1

--- UndergraduateCourses/4GraduationThesis/GBO_Python/Manage.py
+++ UndergraduateCourses/4GraduationThesis/GBO_Python/Manage.py
@@ -1,20 +1,35 @@
 from BenchmarkFunctions import BenchmarkFunctions
 from Optimizer import GBO
+import numpy as np
 
 
 class Manage:
     def __init__(self):
-        pass
+        self.nP = 50
+        self.MaxIt = 50
+        self.fname = "F1"
+        self.bmf = BenchmarkFunctions()
+        self.testNum = 10
+
+    def run_GBO(self):
+        for fname in range(1, self.bmf.size + 1):
+            print("\n%d Problem\n" % (fname))
+            lb, ub, nV, fobj = self.bmf.get(fname)
+            np_Best_Cost = np.zeros([self.testNum])
+            np_Best_X = np.zeros([self.testNum, nV])
+            np_Convergence_curve = np.zeros([self.testNum, self.MaxIt])
+
+            for test_i in range(self.testNum):
+                print("%d test:" % (test_i))
+                gbo = GBO(self.nP, self.MaxIt, lb, ub, nV, fobj)
+                np_Best_Cost[test_i], np_Best_X[test_i], np_Convergence_curve[
+                    test_i] = gbo.get_resault()
+            
+            ave_Best_Cost = np_Best_Cost.mean()
+            print(ave_Best_Cost)
+            input()
 
 
 if __name__ == "__main__":
-    nP = 50
-    MaxIt = 500
-    fname = "F11"
-
-    bf = BenchmarkFunctions()
-    lb, ub, dim, fobj = bf.get(fname)
-
-    gbo = GBO(nP, MaxIt, lb, ub, dim, fobj)
-    Best_Cost, Best_X, Convergence_curve = gbo.get_resault()
-    print(Best_Cost, Best_X)
+    mng = Manage()
+    mng.run_GBO()
