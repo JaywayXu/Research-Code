28a6c6c002e25a360b698b78f5dcfce55338878d
int-lyc <int_lyc@outlook.com>
2021-03-14 02:30:39 +0800

    update

--- 4GraduationThesis/GBO_Python/Manage.py
+++ 4GraduationThesis/GBO_Python/Manage.py
@@ -1,5 +1,6 @@
 from BenchmarkFunctions import BenchmarkFunctions
-from Optimizer import GBO
+from Optimizer.GBO import GBO
+from Optimizer.GA import GA
 # from sko.DE import DE  # use scikit-opt
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 class Manage:
     def __init__(self):
         self.nP = 50
-        self.MaxIt = 500
+        self.MaxIt = 200
         self.bmf = BenchmarkFunctions()
         self.testNum = 20
 
@@ -18,12 +19,30 @@ class Manage:
         lb, ub, nV, fobj = self.bmf.get(fname)
         np_Best_Cost = np.empty([self.testNum])
         np_Best_X = np.empty([self.testNum, nV])
-        # np_Convergence_curve = np.empty([self.testNum, self.MaxIt])
+        np_Convergence_curve = np.empty([self.testNum, self.MaxIt])
 
-        for test_i in range(self.testNum):
+        for i in range(self.testNum):
             gbo = GBO(self.nP, self.MaxIt, lb, ub, nV, fobj)
-            np_Best_Cost[test_i], np_Best_X[test_i] = gbo.run()
-            print("%d test: " % (test_i + 1), np_Best_Cost[test_i])
+            np_Best_Cost[i], np_Best_X[i], np_Convergence_curve[i] = gbo.run()
+            print("%d test: " % (i + 1), np_Best_Cost[i])
+
+        ave_Best_Cost = np_Best_Cost.mean()
+        print("Average Cost:", ave_Best_Cost)
+
+    def run_GA(self, fname):
+        print("GA")
+        lb, ub, nV, fobj = self.bmf.get(fname)
+        np_Best_Cost = np.empty([self.testNum])
+        np_Best_X = np.empty([self.testNum, nV])
+        np_Convergence_curve = np.empty([self.testNum, self.MaxIt])
+
+        for i in range(self.testNum):
+            ga = GA(nP=self.nP, MaxIt=self.MaxIt,
+                    lb=lb, ub=ub, nV=nV, fobj=fobj)
+            np_Best_Cost[i], np_Best_X[i], np_Convergence_curve[i] = ga.run()
+            print("%d test: " % (i + 1), np_Best_Cost[i])
+            ga.Ploterro(np_Convergence_curve[i])
+            input()
 
         ave_Best_Cost = np_Best_Cost.mean()
         print("Average Cost:", ave_Best_Cost)
@@ -31,12 +50,10 @@ class Manage:
     def run_DE(self, fname):
         print("DE")
         lb, ub, nV, fobj = self.bmf.get(fname)
-        lb = np.full(nV, lb)
-        ub = np.full(nV, ub)
         np_Best_Cost = np.empty([self.testNum])
         np_Best_X = np.empty([self.testNum, nV])
 
-        for test_i in range(self.testNum):
+        for i in range(self.testNum):
             de = DE(
                 func=fobj,
                 n_dim=nV,
@@ -45,14 +62,16 @@ class Manage:
                 lb=lb,
                 ub=ub,
             )
-            np_Best_X[test_i], np_Best_Cost[test_i] = de.run()
-            print("%d test: " % (test_i + 1), np_Best_Cost[test_i])
+            np_Best_X[i], np_Best_Cost[i] = de.run()
+            print("%d test: " % (i + 1), np_Best_Cost[i])
 
         ave_Best_Cost = np_Best_Cost.mean()
         print("Average Cost:", ave_Best_Cost)
 
     def drawFunction_2D(self, fname):
         lb, ub, nV, fobj = self.bmf.get(fname)
+        lb = lb[0]
+        ub = ub[0]
 
         figure = plt.figure()
         axes = Axes3D(figure)
@@ -74,7 +93,9 @@ class Manage:
 if __name__ == "__main__":
     mng = Manage()
     fname = 1
-    mng.run_GBO(fname)
+    mng.drawFunction_2D(fname)
+    # mng.run_GBO(fname)
+    # mng.run_GA(fname)
     # mng.run_DE(fname)
     # for i in range(1, 15):
     #     mng.drawFunction_2D(i)
