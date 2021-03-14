d130e620eeda6b27f3b5a596e1e707acca508999
int-lyc <int_lyc@outlook.com>
2021-03-14 17:21:01 +0800

    当前是错误的GA，自然选择算法不能用轮盘赌

--- 4GraduationThesis/GBO_Python/Optimizer/GA.py
+++ 4GraduationThesis/GBO_Python/Optimizer/GA.py
@@ -1,12 +1,11 @@
 import numpy as np
 import copy
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
 class GA:
-    def __init__(self, nP, MaxIt, lb, ub, nV, fobj, lenchrom=20, pc=0.8, pm=0.02):
+    def __init__(self, nP, MaxIt, lb, ub, nV, fobj, lenchrom=10, pc=0.8, pm=0.02):
         self.MaxIt = MaxIt  # Maximum number of iterations
         self.nP = nP  # Number of Population
         self.nV = nV  # Number f Variables
@@ -23,13 +22,25 @@ class GA:
         fitness = np.empty(self.nP)  # 适应度
         errolist = np.empty(self.MaxIt)  # 每代的最优
 
-        best_cost = -1e100
+        best_fitness = -1
         best_pop = np.zeros(self.nV)
         # main loop
         for Current_iter in range(self.MaxIt):
-            print("Iter = " + str(Current_iter))
+            # test
+            pop_v = self.Convert(pop)
+            self.drawPopPoint(pop_v, self.lb, self.ub, self.fobj)
+
+            # print("Iter = " + str(Current_iter))
             pop1 = self.Crossover(pop)
+            # test
+            pop_v = self.Convert(pop1)
+            self.drawPopPoint(pop_v, self.lb, self.ub, self.fobj)
+
             pop2 = self.Mutation(pop1)
+            # test
+            pop_v = self.Convert(pop2)
+            self.drawPopPoint(pop_v, self.lb, self.ub, self.fobj)
+
             pop_v = self.Convert(pop2)
 
             fitness_list = []
@@ -37,27 +48,20 @@ class GA:
                 # 原函数求最小值，所以这里适应值改为相反数 not
                 fitness_list.append(1 / self.fobj(pop_v[j, :]))
             fitness = np.array(fitness_list)
-
-            # test
             pop_v = self.Convert(pop2)
-            # self.drawPop(pop_v, fitness, self.lb, self.ub, self.fobj)
 
             # 自然选择 轮盘赌
             newpop, newfitness = self.Roulette(pop2, fitness)
             best_arg = newfitness.argmax()
-            if best_cost < (newfitness[best_arg]):
-                best_cost = (newfitness[best_arg])
+            if best_fitness < (newfitness[best_arg]):
+                best_fitness = (newfitness[best_arg])
                 best_pop_gene = np.empty((1, self.nV, self.lenchrom))
                 best_pop_gene[0] = newpop[best_arg]
                 best_pop = self.Convert(best_pop_gene)[0]
-            errolist[Current_iter] = 1 / best_cost
+            errolist[Current_iter] = 1 / best_fitness
             pop = copy.deepcopy(newpop)
 
-            # test
-            pop_v = self.Convert(pop)
-            # self.drawPop(pop_v, newfitness, self.lb, self.ub, self.fobj)
-
-        return best_cost, best_pop, errolist
+        return 1 / best_fitness, best_pop, errolist
 
     def InitPop(self):
         '''初始化种群：返回一个三维数组，第一维是种子，第二维是变量维度，第三维是编码基因'''
@@ -136,7 +140,7 @@ class GA:
         # 轮盘赌
         newpop, newfitness = [], []
         for j in range(self.nP):
-        # for j in range(pop.shape[0]):
+            # for j in range(pop.shape[0]):
             p = np.random.randn()
             for k in range(len(accumulation)):
                 if accumulation[k] >= p:
@@ -155,80 +159,42 @@ class GA:
         # return sorted_pop, sorted_fitness
         return newpop, newfitness
 
-    def Ploterro(self, Convergence_curve):
-        '''绘制迭代-误差图'''
-        mpl.rcParams['font.sans-serif'] = ['Courier New']
-        mpl.rcParams['axes.unicode_minus'] = False
-        fig = plt.figure(figsize=(10, 6))
-        x = [i for i in range(len(Convergence_curve))]
-        plt.plot(x, Convergence_curve, 'r-', linewidth=1.5, markersize=5)
-        plt.xlabel(u'Iter', fontsize=18)
-        plt.ylabel(u'Best score', fontsize=18)
-        plt.xticks(fontsize=18)
-        plt.yticks(fontsize=18)
-        plt.xlim(0, )
-        plt.grid(True)
-        plt.show()
-
-    def drawPop(self, pop_v, fitness, lb, ub, fobj):
-        # # fobj
-        # X = np.arange(lb[0], ub[0], 1)
-        # Y = np.arange(lb[1], ub[1], 1)
-        # X, Y = np.meshgrid(X, Y)
-        # Z = np.zeros(X.shape)
-        # for i in range(X.shape[0]):
-        #     for j in range(X.shape[1]):
-        #         Z[i, j] = fobj(np.array([X[i, j], Y[i, j]]))
-        # plt.contourf(X, Y, Z)
-        # plt.contour(X, Y, Z)
-
-        # # pop
-        # pop_t = pop_v.T
-        # x = pop_t[0, :]
-        # y = pop_t[1, :]
-        # plt.scatter(x, y)
-        # plt.show()
-
+    def drawPopPoint(self, pop_v, lb, ub, fobj):
         # fobj
         figure = plt.figure()
         axes = Axes3D(figure)
-
-        lb = lb[0]
-        ub = ub[0]
-        X = np.arange(lb, ub, 1)
-        Y = np.arange(lb, ub, 1)
+        X = np.arange(lb[0], ub[0], 0.2)
+        Y = np.arange(lb[1], ub[1], 0.2)
         X, Y = np.meshgrid(X, Y)
         Z = np.zeros(X.shape)
         for i in range(X.shape[0]):
             for j in range(X.shape[1]):
                 Z[i, j] = fobj(np.array([X[i, j], Y[i, j]]))
-        axes.plot_surface(X, Y, Z, cmap='rainbow')
+        axes.plot_surface(X, Y, Z, cmap='rainbow', alpha=.3)
 
         # pop
+        z = np.empty(pop_v.shape[0])
+        for i in range(pop_v.shape[0]):
+            z[i] = fobj(pop_v[i, :])
         pop_t = pop_v.T
         x = pop_t[0, :]
         y = pop_t[1, :]
-        z = 1 / fitness
-        print(x.shape, y.shape, z.shape)
+        z = z.T
         axes.scatter(x, y, z, c='r', marker='.')
-
         plt.show()
 
 
 if __name__ == '__main__':
     nP = 50
-    MaxIt = 300
-    nV = 2
-    lb = [-100 for _ in range(nV)]
-    ub = [100 for _ in range(nV)]
+    MaxIt = 200
 
-    def f1(x):
-        '''Bent Cigar (Unimodal)'''
-        D = x.shape[0]
-        z = np.power(x[1], 2) + pow(10, 6) * np.power(x[2:D], 2).sum()
-        return z
+    import sys
+    sys.path.append("..")
+    from BenchmarkFunctions import BenchmarkFunctions
+    bmf = BenchmarkFunctions()
+    lb, ub, nV, fobj = bmf.get(1)
 
-    ga = GA(nP=nP, MaxIt=MaxIt, lb=lb, ub=ub, nV=nV, fobj=f1)
+    ga = GA(nP=nP, MaxIt=MaxIt, lb=lb, ub=ub, nV=nV, fobj=fobj)
     Best_Cost, Best_X, Convergence_curve = ga.run()
     print(Best_Cost, Best_X, Convergence_curve)
     ga.Ploterro(Convergence_curve)
