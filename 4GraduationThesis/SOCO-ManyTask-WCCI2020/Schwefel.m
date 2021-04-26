147a3f52f3ca3ce6f850459cd2826a959f66b28c
int-lyc <int_lyc@outlook.com>
2021-04-26 16:00:33 +0800

    benchmark

---
+++ 4GraduationThesis/SOCO-ManyTask-WCCI2020/Schwefel.m
@@ -0,0 +1,15 @@
+function obj = Schwefel(x,M,opt)
+%SCHWEFEL function
+%   - var: design variable vector
+    var = x;
+    dim = length(var);
+    var = (M*(var-opt)')';
+    sum = 0;
+    for i = 1: dim
+        sum = sum + var(i)*sin(sqrt(abs(var(i))));
+    end
+    
+    obj = 418.9829*dim-sum;
+    
+end
+
---
+++ 4GraduationThesis/SOCO-ManyTask-WCCI2020/Weierstrass.m
@@ -0,0 +1,21 @@
+function obj = Weierstrass(x,M,opt)
+%WEIERSTASS function
+%   - var: design variable vector
+%   - M: rotation matrix
+%   - opt: shift vector
+    var = x;
+    D = length(var);
+    var = (M*(var-opt)')';
+    a = 0.5;
+    b = 3;
+    kmax = 20;
+    obj = 0;
+    for i = 1:D
+        for k = 0:kmax
+            obj = obj + a^k*cos(2*pi*b^k*(var(i)+0.5));
+        end
+    end
+    for k = 0:kmax
+        obj = obj - D*a^k*cos(2*pi*b^k*0.5);
+    end
+end
