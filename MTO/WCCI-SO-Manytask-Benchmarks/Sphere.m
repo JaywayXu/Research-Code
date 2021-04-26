3e6852483255219db042c84f8d003e7c04800fc0
int-lyc <int_lyc@outlook.com>
2021-04-26 15:40:09 +0800

    benchmark wcci so many-task

---
+++ MTO/WCCI-SO-Manytask-Benchmarks/Sphere.m
@@ -0,0 +1,15 @@
+function obj = Sphere(x,M,opt)
+%Sphere function
+%   - var: design variable vector
+%   - M: rotation matrix
+%   - opt: shift vector
+    var = x;
+    dim = length(var);
+    var = (M*(var-opt)')';
+    sum = 0;
+    for i = 1: dim
+        sum = sum + var(i)*var(i);
+    end
+    obj = sum;    
+end
+
