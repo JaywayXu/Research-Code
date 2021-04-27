692e6a77aba741c4dc1f37559e16f53ffadc73ff
int-lyc <int_lyc@outlook.com>
2021-04-27 17:04:02 +0800

    修改

--- 4GraduationThesis/SOCO-2016/MAIN.m
+++ 4GraduationThesis/SOCO-2016/MAIN.m
@@ -1,13 +1,12 @@
-pop_M = 100; % population size 100
+pop_M = 100; % population size
 pop_S = pop_M;
-gen = 500; % generation count 1000
+gen = 500; % generation count
 p_il = 0; % probability of individual learning (BFGA quasi-Newton Algorithm) --> Indiviudal Learning is an IMPORTANT component of the MFGBO.
 rmp = 0.3; % random mating probability
 pr = 0.5; % Probability Parameter
-reps = 30; % repetitions 20
-
+reps = 30; % repetitions
 benchmark_num = 9;
-is_run = false; % 是否运行，false则直接读取mat文件
+is_run = 1; % 是否运行，false则直接读取mat文件
 
 if is_run
 
@@ -16,37 +15,27 @@ if is_run
         Tasks = benchmark(index);
         data_MFGBO(index) = MFGBO(Tasks, pop_M, gen, rmp, pr, p_il, reps);
 
-        % "task_for_comparison_with_SOO" compares performance of corresponding task in MFO with SOO.
-        % For Instance, In EXAMPLE 1 ...
-        % "task_for_comparison_with_SOO" = 1 --> compares 40-D Rastrin in MFO with 40-D
-        % Rastrigin in SOO.
-        % "task_for_comparison_with_SOO" = 2 --> compares 30D Ackley in MFO with
-        % 30D Ackley in SOO.
-        task_for_comparison_with_SOO = 1;
-        data_SOO_1(index) = SOGBO(Tasks(task_for_comparison_with_SOO), pop_S, gen, pr, p_il, reps);
+        for task_i = 1:size(Tasks, 2)
+            disp(['SOTask: ', num2str(task_i)]);
+            data_SOO(task_i, index) = SOGBO(Tasks(task_i), pop_S, gen, pr, p_il, reps);
+        end
 
-        task_for_comparison_with_SOO = 2;
-        data_SOO_2(index) = SOGBO(Tasks(task_for_comparison_with_SOO), pop_S, gen, pr, p_il, reps);
-
-        data_EA(index) = MFEA(Tasks, pop_M, gen, 'elitist', rmp, p_il, reps);
+        data_GA(index) = MFEA(Tasks, pop_M, gen, 'elitist', rmp, p_il, reps);
         data_DE(index) = MFDE(Tasks, pop_M, gen, 'elitist', rmp, p_il, reps);
         data_PSO(index) = MFPSO(Tasks, pop_M, gen, rmp, p_il, reps);
     end
 
-    % draw
     save('data_MFGBO', 'data_MFGBO');
-    save('data_SOO_1', 'data_SOO_1');
-    save('data_SOO_2', 'data_SOO_2');
-    save('data_EA', 'data_EA');
+    save('data_SOO', 'data_SOO');
+    save('data_GA', 'data_GA');
     save('data_DE', 'data_DE');
     save('data_PSO', 'data_PSO');
 end
 
 load('data_MFGBO', 'data_MFGBO');
-load('data_SOO_1', 'data_SOO_1');
-load('data_SOO_2', 'data_SOO_2');
-load('data_EA', 'data_EA');
+load('data_SOO', 'data_SOO');
+load('data_GA', 'data_GA');
 load('data_DE', 'data_DE');
 load('data_PSO', 'data_PSO');
-convergeTrend(data_MFGBO, data_SOO_1, data_SOO_2, reps, gen, benchmark_num);
-convergeTrend_compare(data_MFGBO, data_EA, data_DE, data_PSO, reps, gen, benchmark_num);
+convergeTrend(data_MFGBO, data_SOO, reps, gen, benchmark_num, 2);
+convergeTrend_compare(data_MFGBO, data_GA, data_DE, data_PSO, reps, gen, benchmark_num, 2);
--- 4GraduationThesis/SOCO-ManyTask-WCCI2020/MAIN.m
+++ 4GraduationThesis/SOCO-ManyTask-WCCI2020/MAIN.m
@@ -1,16 +1,16 @@
-pop_M = 100; % population size
-pop_S = pop_M;
+pop_M = 200; % population size
+pop_S = 50;
 gen = 1000; % generation count
 rmp = 0.3; % random mating probability
 pr = 0.5; % Probability Parameter
-reps = 30; % repetitions
+reps = 4; % repetitions
 benchmark_num = 10;
 is_run = 1; % 是否运行，false则直接读取mat文件
 
 if is_run
 
     for index = 1:benchmark_num
-        disp(['Benchmark: ', num2str(index)])
+        disp(['Benchmark: ', num2str(index)]);
         Tasks = benchmark(index);
         data_MFGBO(index) = MFGBO(Tasks, pop_M, gen, rmp, pr, reps);
 
@@ -19,22 +19,13 @@ if is_run
             data_SOO(task_i, index) = SOGBO(Tasks(task_i), pop_S, gen, pr, reps);
         end
 
-        data_GA(index) = MFEA(Tasks, pop_M, gen, 'elitist', rmp, reps);
-        data_DE(index) = MFDE(Tasks, pop_M, gen, 'elitist', rmp, reps);
-        data_PSO(index) = MFPSO(Tasks, pop_M, gen, rmp, reps);
+        % data_MFGA(index) = MFEA(Tasks, pop_M, gen, 'elitist', rmp, reps);
     end
 
     save('data_MFGBO', 'data_MFGBO');
     save('data_SOO', 'data_SOO');
-    save('data_GA', 'data_GA');
-    save('data_DE', 'data_DE');
-    save('data_PSO', 'data_PSO');
 end
 
 load('data_MFGBO', 'data_MFGBO');
 load('data_SOO', 'data_SOO');
-load('data_GA', 'data_GA');
-load('data_DE', 'data_DE');
-load('data_PSO', 'data_PSO');
-convergeTrend(data_MFGBO, data_SOO, reps, gen, benchmark_num, 2);
-convergeTrend_compare(data_MFGBO, data_GA, data_DE, data_PSO, reps, gen, benchmark_num)
+convergeTrend(data_MFGBO, data_SOO, reps, gen, benchmark_num, 50);
