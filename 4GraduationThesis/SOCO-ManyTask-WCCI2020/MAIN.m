cfc6d6c40cf34429f30ba6cd1a42d343b432733d
int-lyc <int_lyc@outlook.com>
2021-05-07 17:09:13 +0800

    1

--- 4GraduationThesis/SOCO-ManyTask-WCCI2020/MAIN.m
+++ 4GraduationThesis/SOCO-ManyTask-WCCI2020/MAIN.m
@@ -3,17 +3,17 @@ pop_S = 50;
 gen = 1000; % generation count
 rmp = 0.3; % random mating probability
 pr = 0.5; % Probability Parameter
-reps = 2; % repetitions
+reps = 30; % repetitions
 benchmark_num = 10;
-is_run = 0; % 是否运行，false则直接读取mat文件
+is_run = 1; % 是否运行，false则直接读取mat文件
 
 if is_run
 
     for index = 1:benchmark_num
         disp(['Benchmark: ', num2str(index)]);
         Tasks = benchmark(index);
-        data_MFGBO(index) = MFGBO(Tasks, pop_M, gen, rmp, pr, reps);
-        data_MFGBO_2(index) = MFGBO_2(Tasks, pop_M, gen, rmp, pr, reps);
+        % data_MFGBO(index) = MFGBO(Tasks, pop_M, gen, rmp, pr, reps);
+        data_MFGBO(index) = MFGBO_2(Tasks, pop_M, gen, rmp, pr, reps);
         % for task_i = 1:size(Tasks, 2)
         %     disp(['SOTask: ', num2str(task_i)]);
         %     data_SOO(task_i, index) = SOGBO(Tasks(task_i), pop_S, gen, pr, reps);
@@ -23,12 +23,12 @@ if is_run
     end
 
     save('data_MFGBO', 'data_MFGBO');
-    save('data_MFGBO_2', 'data_MFGBO_2');
+    % save('data_MFGBO_2', 'data_MFGBO_2');
     % save('data_SOO', 'data_SOO');
 end
 
 load('data_MFGBO', 'data_MFGBO');
-load('data_MFGBO_2', 'data_MFGBO_2');
-% load('data_SOO', 'data_SOO');
-% convergeTrend(data_MFGBO, data_SOO, reps, gen, benchmark_num, 50);
-convergeTrend_2(data_MFGBO, data_MFGBO_2, reps, gen, benchmark_num, 50);
+% load('data_MFGBO_2', 'data_MFGBO_2');
+load('data_SOO', 'data_SOO');
+convergeTrend(data_MFGBO, data_SOO, reps, gen, benchmark_num, 50);
+% convergeTrend_2(data_MFGBO, data_MFGBO_2, reps, gen, benchmark_num, 50);
