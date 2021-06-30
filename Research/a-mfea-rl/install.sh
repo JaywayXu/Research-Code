5b8b5f9dd9b2beb21ade5056611b3ada1bddbf58
int-lyc <int_lyc@outlook.com>
2021-06-30 16:34:36 +0800

    remove

--- Research/a-mfea-rl/install.sh
+++
@@ -1,8 +0,0 @@
-# Import conda environment
-conda env create -f amfearl.yml
-conda activate amfearl
-
-# Clone and install Metaworld
-git clone https://github.com/rlworkgroup/metaworld
-cd metaworld
-pip install -e .
