90fc64174514942f0ecf4c1bbcf36caeb0b20ec1
int-lyc <int_lyc@outlook.com>
2021-07-21 21:12:09 +0800

    2D function task

--- Test/SOMTO_Figure_MTSO/MAIN_draw.m
+++ Test/SOMTO_Figure_MTSO/MAIN_draw.m
@@ -1,32 +1,45 @@
 % 画出每个测试用例的每个函数在1Dim上的图像
 BM_NUM = 9;
 benchNameList = {'CI-HS', 'CI-MS', 'CI-LS', 'PI-HS', 'PI-MS', 'PI-LS', 'NI-HS', 'NI-MS', 'NI-LS'};
+lineColor = {'blue', 'red'};
+markerColor = {'blue', 'red'};
+
+mkdir('./Results_Figure')
 
 for index = 1:BM_NUM
-    Tasks = benchmark(index);
+    Tasks = benchmark(index, 1);
     no_of_tasks = length(Tasks);
 
-    fig = figure('Visible', 'on');
-    x = 0:1/100:1;
-    y = x;
-    [xx, yy] = meshgrid(x, y);
-    f = zeros(size(xx));
+    fig = figure('Visible', 'off');
+    x = 0:1/200:1;
+    f = zeros(size(x));
 
     for no = 1:no_of_tasks
 
-        for i = 1:size(xx, 1)
-
-            for j = 1:size(xx, 2)
-                f(i, j) = fnceval(Tasks(no), [xx(i, j), yy(i, j)]);
-            end
-
+        for i = 1:length(x)
+            f(i) = fnceval(Tasks(no), x(i));
         end
 
-        mesh(xx, yy, f, ...
-            'LineWidth', 1)
+        % 绘制曲线
+        fmin = min(f);
+        fmax = max(f);
+        f = (f - fmin) / (fmax - fmin);
+        plot(x, f, ...
+            'LineWidth', 1, ...
+            'Color', lineColor{no})
+        hold on
+
+        % 绘制最低点
+        xmin = x(f == min(f)); % f最小值所对应的x坐标
+        fmin = min(f);
+        plot(xmin, fmin, 'o', ...
+            'MarkerEdgeColor', markerColor{no})
+
         hold on
     end
 
     title(benchNameList{index})
-    legend(['Task1', 'Task2'])
+    legend('Task1 Line', 'Task1 Min Point', 'Task2 Line', 'Task2 Min Point')
+
+    saveas(fig, ['./Results_Figure/', int2str(index), '_', benchNameList{index}, '.png'])
 end
