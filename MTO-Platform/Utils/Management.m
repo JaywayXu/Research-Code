3de179d93dd1040da17d68b463d044857ab4407f
intLyc <int_lyc@outlook.com>
2021-09-18 14:54:24 +0800

    1

--- MTO-Platform/Utils/Management.m
+++ MTO-Platform/Utils/Management.m
@@ -12,11 +12,11 @@ classdef Management
 
         function obj = Management(varargin)
 
-            if nargin == 5
+            if nargin == 4
                 obj.reps = varargin{1};
-                obj.pop_size = varargin{3};
-                obj.iter_num = varargin{4};
-                obj.eva_num = varargin{5};
+                obj.pop_size = varargin{2};
+                obj.iter_num = varargin{3};
+                obj.eva_num = varargin{4};
             end
 
         end
@@ -44,13 +44,23 @@ classdef Management
 
             % main test loop
             for prob = 1:length(Prob_list)
+                app.printlog(['<- Problem: ', Prob_list{prob}, ' ->']);
 
                 for rep = 1:obj.reps
+                    app.printlog(['Rep: ', num2str(rep)]);
 
                     for algo = 1:length(Algo_list)
+                        app.printlog([Algo_list{algo}]);
                         data = obj.singleRun(Algo_list{algo}, Prob_list{prob});
                         obj.result(algo, prob).clock_time = obj.result(algo, prob).clock_time + data.clock_time;
                         obj.result(algo, prob).convergence = [obj.result(algo, prob).convergence; data.convergence];
+
+                        % app.update()
+
+                        if app.isCancel()
+                            app.printlog('!!! Canceled !!!');
+                            return;
+                        end
 
                     end
 
@@ -60,7 +70,6 @@ classdef Management
 
             % save the data
             savedata.reps = obj.reps;
-            savedata.no_of_tasks = obj.no_of_tasks;
             savedata.pop_size = obj.pop_size;
             savedata.iter_num = obj.iter_num;
             savedata.algo_list = Algo_list';
